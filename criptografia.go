49989f5faf33ebbe270340946fa0069446f2b370
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-09-28 15:03:54 -0300

    Fim da atividade. A parte da descriptografia não ficou bem como esperada. Tente modificar/melhorar em outro momento.

--- criptografia.go
+++ criptografia.go
@@ -1,1 +1,46 @@
-// batata
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Digite uma mensagem qualquer:")
+	// Scanner usado para conseguir pegar mensagens com espaços entre palavras.
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	texto := scanner.Text()
+	fmt.Scanln(&texto)
+	fmt.Println("\nDigite a cifra desejada:")
+	var cifra rune = 0
+	fmt.Scanln(&cifra)
+
+	cripto := func(r rune) rune {
+		switch {
+		case r >= 'A' && r <= 'Z':
+			return 'A' + (r-'A'+cifra)%26
+		case r >= 'a' && r <= 'z':
+			return 'a' + (r-'a'+cifra)%26
+		}
+		return r
+	}
+
+	descripto := func(r rune) rune {
+		switch {
+		case r >= 'A' && r <= 'Z':
+			return 'A' + (r-'A'-cifra)%26
+		case r >= 'a' && r <= 'z':
+			return 'a' + (r-'a'-cifra)%26
+		}
+		return r
+	}
+
+	fmt.Println("\nCifra utilizada: \n", cifra)
+	fmt.Println("\nTexto criptografado: \n", strings.Map(cripto, texto))
+	fmt.Println("\nTexto descriptografado: \n", strings.Map(descripto, strings.Map(cripto, texto)))
+}
+
+// Não ficou bem como desejado. Tentarei em outras linguagens depois.
---
+++ readme.txt
@@ -0,0 +1,23 @@
+Sobre a atividade em si:
+
+"Desenvolver um programa que realize a cifra de César.
+Ele deverá conter preferencialmente:
+Uma função que faz criptografia lendo um texto claro e o transforme em texto
+cifrado, substituindo os caracteres originais por caracteres que estão “X&quot;
+posições a frente.
+Você terá o programa principal (main) e duas funções (encripta e decripta).
+No programa principal você deverá:
+1. Informar o texto claro;
+2. Informar o valor (numérico) da cifra (quantas casa a frente será a letra
+criptografada;
+3. Chamada das funções encripta e decripta;
+4. Imprimir em tela o texto criptografado;
+5. Imprimir o descriptografado;
+Na funções (encripta e decripta).
+Encripta:
+1. Informar o texto claro;
+2. Informar o valor (numérico) da cifra (quantas casa a frente será a letra
+criptografada;
+3. Chamada das funções encripta e decripta;
+4. Imprimir em tela o texto criptografado;
+Imprimir o descriptografado;"
